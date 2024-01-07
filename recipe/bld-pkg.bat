95b08bf0958217b9eed0244c8ac7314f7234086e
Tobias Fischer <info@tobiasfischer.info>
2024-01-08 09:38:52 +1000

    Try set VS170COMNTOOLS
    https://gitlab.kitware.com/cmake/cmake/-/issues/24183

--- recipe/bld-pkg.bat
+++ recipe/bld-pkg.bat
@@ -8,6 +8,8 @@ for /f "tokens=* usebackq" %%f in (`where cl.exe`) do (set "dummy=%%f" && call s
 
 echo %CC%
 
+set VS170COMNTOOLS=C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\
+
 %PYTHON% %RECIPE_DIR%\builder.py
 
 if errorlevel 1 exit 1
