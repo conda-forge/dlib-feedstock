ea4ae8763ff2f01899fe33ec51e08c4c69676789
Tobias Fischer <info@tobiasfischer.info>
2024-01-07 12:44:16 +1000

    Use script to set CC
    See https://github.com/conda-forge/tomocupy-feedstock/commit/2d6fb38e95c1a968eec6b3d865aeef451d32064c

--- recipe/bld-pkg.bat
+++ recipe/bld-pkg.bat
@@ -1,5 +1,13 @@
 @echo on
 
+for /f "tokens=* usebackq" %%f in (`where cl.exe`) do (set "dummy=%%f" && call set "CXX=%%dummy:\=\\%%")
+
+echo %CXX%
+
+for /f "tokens=* usebackq" %%f in (`where cl.exe`) do (set "dummy=%%f" && call set "CC=%%dummy:\=\\%%")
+
+echo %CC%
+
 %PYTHON% %RECIPE_DIR%\builder.py
 
 if errorlevel 1 exit 1
