8ab0bc7323f523d76a7da9f1d3900d8526bb99e9
Tobias Fischer <info@tobiasfischer.info>
2024-01-08 09:54:54 +1000

    Cleanup

--- recipe/bld-pkg.bat
+++ recipe/bld-pkg.bat
@@ -1,17 +1,5 @@
 @echo on
 
-for /f "tokens=* usebackq" %%f in (`where cl.exe`) do (set "dummy=%%f" && call set "CXX=%%dummy:\=\\%%")
-
-echo %CXX%
-
-for /f "tokens=* usebackq" %%f in (`where cl.exe`) do (set "dummy=%%f" && call set "CC=%%dummy:\=\\%%")
-
-echo %CC%
-
-set VS170COMNTOOLS=C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\
-
-dir "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\"
-
 %PYTHON% %RECIPE_DIR%\builder.py
 
 if errorlevel 1 exit 1
