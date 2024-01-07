fc21a517788a215bef9e2cc856356571c4c6c830
Tobias Fischer <info@tobiasfischer.info>
2024-01-08 09:39:28 +1000

    Debug

--- recipe/bld-pkg.bat
+++ recipe/bld-pkg.bat
@@ -10,6 +10,8 @@ echo %CC%
 
 set VS170COMNTOOLS=C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\
 
+dir "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\"
+
 %PYTHON% %RECIPE_DIR%\builder.py
 
 if errorlevel 1 exit 1
